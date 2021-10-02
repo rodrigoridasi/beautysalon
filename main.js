d81ec8095f551f4e3950c3f4d8d7ff657f7b26c0
Rodrigo R. da Silva <rodrigoridasi@gmail.com>
2021-10-02 13:39:37 -0300

    Fixing bugs and adding responsivity

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-/* abre e fecha o menu quando clicar no icone: hamburguer e x */
+/* Abre e fecha o menu quando clicar no icone: hamburguer e x. */
 const nav = document.querySelector('#header nav')
 const toggle = document.querySelectorAll('nav .toggle')
 
@@ -8,7 +8,7 @@ for (const element of toggle) {
   })
 }
 
-/* quando clicar em um item do menu, esconder o menu */
+/* Quando clicar em um item do menu, esconder o menu. */
 const links = document.querySelectorAll('nav ul li a')
 
 for (const link of links) {
@@ -17,31 +17,37 @@ for (const link of links) {
   })
 }
 
-/* mudar o header da página quando der scroll */
-function changeHeaderWhenScroll() {
-  const header = document.querySelector('#header')
-  const navHeight = header.offsetHeight
+/* Mudar o header da página quando der scroll. */
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
 
+function changeHeaderWhenScroll() {
   if (window.scrollY >= navHeight) {
-    // scroll é maior que a altura do header
+    // Scroll é maior que a altura do header.
     header.classList.add('scroll')
   } else {
-    // menor que a altura do header
+    // ...menor que a altura do header.
     header.classList.remove('scroll')
   }
 }
 
-/* Testimonials carousel slider swiper */
+/* Testimonials carousel slider swiper. */
 const swiper = new Swiper('.swiper-container', {
   slidesPerView: 1,
   pagination: {
     el: '.swiper-pagination'
   },
   mousewheel: true,
-  keyboard: true
+  keyboard: true,
+  breakpoints: {
+    767: {
+      slidesPerView: 2,
+      setWrapperSize: true
+    }
+  }
 })
 
-/* ScrollReveal: Mostrar elementos quando der scroll na página */
+/* ScrollReveal: Mostrar elementos quando der scroll na página. */
 const scrollReveal = ScrollReveal({
   origin: 'top',
   distance: '30px',
@@ -60,10 +66,10 @@ scrollReveal.reveal(
   { interval: 100 }
 )
 
-/* Botão voltar para o topo */
-function backToTop() {
-  const backToTopButton = document.querySelector('.back-to-top')
+/* Botão voltar para o topo. */
+const backToTopButton = document.querySelector('.back-to-top')
 
+function backToTop() {
   if (window.scrollY >= 560) {
     backToTopButton.classList.add('show')
   } else {
@@ -71,8 +77,34 @@ function backToTop() {
   }
 }
 
+/* Menu ativo conforme a seção visível na página. */
+const sections = document.querySelectorAll('main section[id]')
+function activateMenuAtCurrentSection() {
+  const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4
+
+  for (const section of sections) {
+    const sectionTop = section.offsetTop
+    const sectionHeight = section.offsetHeight
+    const sectionId = section.getAttribute('id')
+
+    const checkpointStart = checkpoint >= sectionTop
+    const checkpointEnd = checkpoint <= sectionTop + sectionHeight
+
+    if (checkpointStart && checkpointEnd) {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.add('active')
+    } else {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.remove('active')
+    }
+  }
+}
+
 /* When Scroll */
 window.addEventListener('scroll', function () {
   changeHeaderWhenScroll()
   backToTop()
+  activateMenuAtCurrentSection()
 })
